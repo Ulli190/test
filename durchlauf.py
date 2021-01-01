5509833a85dbc7db421939524ee5a3744317f12e
Peter Pan <ulrich.kuehlenborg@gmaik.com>
2021-01-01 01:14:44 +0100

    first

---
+++ durchlauf.py
@@ -0,0 +1,11 @@
+def durchlauf(satz1,satz2):
+    ausgabe = ""
+    while len(satz1) < len(satz2):
+        satz1 += " "
+    for i in range(len(satz1)):
+        ausgabe = satz1[i] + "     " + satz2[i]
+        print(ausgabe)
+while input() != "x":
+    eingabe1 = input("Bitte schreiben \n:")
+    eingabe2 = input("Schreibe noch etwas \n:")
+    durchlauf(eingabe1, eingabe2)
